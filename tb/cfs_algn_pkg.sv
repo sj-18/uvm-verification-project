ecf3a30d31e4440d8049809486ead41fa0354501
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-20 13:57:21 +0530

    Create cfs_algn_pkg.sv

---
+++ tb/cfs_algn_pkg.sv
@@ -0,0 +1,17 @@
+`ifndef CFS_ALGN_PKG_SV
+ `define CFS_ALGN_PKG_SV
+
+`include "uvm_macros.svh"
+`include "cfs_apb_pkg.sv"
+
+package cfs_algn_pkg;
+
+import uvm_pkg::*;
+import cfs_apb_pkg::*;
+
+`include "cfs_algn_env.sv"
+
+
+endpackage
+
+`endif
