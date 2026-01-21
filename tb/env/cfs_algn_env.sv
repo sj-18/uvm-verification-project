1812f84e8b09eea1699dc2af0687ae43b9635e1e
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-21 17:40:14 +0530

    Update cfs_algn_env.sv

--- tb/env/cfs_algn_env.sv
+++ tb/env/cfs_algn_env.sv
@@ -2,11 +2,20 @@
  `define CFS_ALGN_ENV_SV
 
 class cfs_algn_env extends uvm_env;
-    
+  
+  //Create instance of apb_agent
+  cfs_apb_agent apb_agent;
+  
   `uvm_component_utils(cfs_algn_env)
   
   function new(string name = "", uvm_component parent);
     super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    apb_agent = cfs_apb_agent::type_id::create("apb_agent", this);
   endfunction
   
 endclass
