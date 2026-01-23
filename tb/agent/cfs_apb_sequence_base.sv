adb4cb560ddc5a3e5ac791856d4c8609e5e6c7dd
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-23 19:53:15 +0530

    Create cfs_apb_sequence_base.sv

---
+++ tb/agent/cfs_apb_sequence_base.sv
@@ -0,0 +1,18 @@
+`ifndef CFS_APB_SEQUENCE_BASE_SV
+ `define CFS_APB_SEQUENCE_BASE_SV
+
+class cfs_apb_sequence_base extends uvm_sequence#(.REQ(cfs_apb_item_drv));
+  
+  //Create p_sequencer
+  `uvm_declare_p_sequencer(cfs_apb_sequencer)
+  
+  `uvm_object_utils(cfs_apb_sequence_base)
+  
+  function new(string name = "");
+    super.new(name);    
+  endfunction
+  
+endclass
+
+
+`endif
