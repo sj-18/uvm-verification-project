70bc8295ae53b31728aba018e4987bd411e2967e
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-23 19:54:17 +0530

    Update cfs_apb_pkg.sv

--- tb/agent/cfs_apb_pkg.sv
+++ tb/agent/cfs_apb_pkg.sv
@@ -12,7 +12,14 @@ import uvm_pkg::*;
 `include "cfs_apb_item_base.sv"
 `include "cfs_apb_item_drv.sv"
 `include "cfs_apb_agent_config.sv"
+`include "cfs_apb_sequencer.sv"
+`include "cfs_apb_driver.sv"
 `include "cfs_apb_agent.sv"
+
+`include "cfs_apb_sequence_base.sv"
+`include "cfs_apb_sequence_simple.sv"
+`include "cfs_apb_sequence_rw.sv"
+`include "cfs_apb_sequence_random.sv"
 
 endpackage
 
