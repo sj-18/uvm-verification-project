7be7a2f9a0dd284474e5e535a1e9ba369ffbf6d9
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-24 21:00:44 +0530

    Update cfs_apb_driver.sv

--- tb/agent/cfs_apb_driver.sv
+++ tb/agent/cfs_apb_driver.sv
@@ -28,6 +28,8 @@ class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv));
     vif.paddr   <= 0;
     vif.pwdata  <= 0;
     
+    repeat(5) @(posedge vif.pclk);
+    
     //Drive transactions after reset
     forever begin
       cfs_apb_item_drv item;
@@ -41,16 +43,19 @@ class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv));
   endtask
   
   
+  //Logic to drive the APB transaction as per protocol
   protected virtual task drive_transaction(cfs_apb_item_drv item);
     
     cfs_apb_vif vif = agent_config.get_vif();
     
     `uvm_info("DEBUG", $sformatf("Driving \"%s\" : %s", item.get_full_name(), item.convert2string()) ,UVM_NONE);
     
+    //Pre_drive delay before the APB transaction
     for(int i=0; i<item.pre_drive_delay; i++) begin
       @(posedge vif.pclk);
     end
     
+    //Setup phase start
     vif.psel   <= 1'b1;
     vif.paddr  <= item.addr;
     vif.pwrite <= bit'(item.dir);
@@ -59,9 +64,31 @@ class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv));
       vif.pwdata <= item.data;
     end
     
-    @(posedge vif.clk);
+    //After this clk edge we enter into setup phase
+    @(posedge vif.pclk);
     
+    //Access phase start
     vif.penable <= 1'b1;
+    
+    //After this clk edge we enter into access phase
+    @(posedge vif.pclk);
+    
+    //Wait for slave to be ready after which transaction is over in the same cycle
+    while(vif.pready != 1) begin
+      @(posedge vif.pclk);
+    end
+    
+    //After the transaction drive back all signals to 0
+    vif.psel    <= 0;
+    vif.penable <= 0;
+    vif.pwrite  <= 0;
+    vif.paddr   <= 0;
+    vif.pwdata  <= 0;    
+    
+    //Post_drive delay after the ABP transaction
+    for(int i=0; i<item.post_drive_delay; i++) begin
+      @(posedge vif.pclk);
+    end
     
   endtask
   
