0d115a13404fb7f6766090e8dae6613428130732
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-27 15:42:39 +0530

    Update cfs_apb_item_base.sv - Moved common fields between drv and mon  items here

--- tb/agent/cfs_apb_item_base.sv
+++ tb/agent/cfs_apb_item_base.sv
@@ -4,11 +4,21 @@
 
 //shall include driver and monitor driving items
 class cfs_apb_item_base extends uvm_sequence_item;
+    
+  rand cfs_apb_dir dir;
+  rand cfs_apb_data data;
+  rand cfs_apb_addr addr;
   
   `uvm_object_utils(cfs_apb_item_base)
   
   function new(string name="");
     super.new(name);
+  endfunction
+  
+    virtual function string convert2string();
+    string result = $sformatf("dir: %0s, addr: %0x", dir.name(), addr);
+    
+    return result;
   endfunction
   
 endclass
