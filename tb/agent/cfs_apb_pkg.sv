76276d524a7d2f36e6be496818f711b2959a5726
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-01-27 15:38:11 +0530

    Update cfs_apb_pkg.sv - added monitor

--- tb/agent/cfs_apb_pkg.sv
+++ tb/agent/cfs_apb_pkg.sv
@@ -11,9 +11,11 @@ import uvm_pkg::*;
 `include "cfs_apb_types.sv"
 `include "cfs_apb_item_base.sv"
 `include "cfs_apb_item_drv.sv"
+`include "cfs_apb_item_mon.sv"
 `include "cfs_apb_agent_config.sv"
 `include "cfs_apb_sequencer.sv"
 `include "cfs_apb_driver.sv"
+`include "cfs_apb_monitor.sv"
 `include "cfs_apb_agent.sv"
 
 `include "cfs_apb_sequence_base.sv"
