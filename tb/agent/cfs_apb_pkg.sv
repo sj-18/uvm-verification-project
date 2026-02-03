d9f56dbe4c9e8696f783ea9e42fdafa677e08edd
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-02-03 11:12:23 +0530

    Update cfs_apb_pkg.sv - Added coverage file

--- tb/agent/cfs_apb_pkg.sv
+++ tb/agent/cfs_apb_pkg.sv
@@ -14,6 +14,7 @@ import uvm_pkg::*;
 `include "cfs_apb_item_mon.sv"
 `include "cfs_apb_agent_config.sv"
 `include "cfs_apb_sequencer.sv"
+`include "cfs_apb_coverage.sv"
 `include "cfs_apb_driver.sv"
 `include "cfs_apb_monitor.sv"
 `include "cfs_apb_agent.sv"
