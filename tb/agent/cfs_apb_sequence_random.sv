f248e45b5295d045216c2a33e1bb0fc382cd231f
sj-18 <68154073+sj-18@users.noreply.github.com>
2026-03-06 13:58:34 +0530

    Update cfs_apb_sequence_random.sv - Clean-up + Comments

--- tb/agent/cfs_apb_sequence_random.sv
+++ tb/agent/cfs_apb_sequence_random.sv
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////////////////////////////////////
+// Description: APB Random sequence. This sends the simple sequence num_items times.
+///////////////////////////////////////////////////////////////////////////////
+
 `ifndef CFS_APB_SEQUENCE_RANDOM_SV
  `define CFS_APB_SEQUENCE_RANDOM_SV
 
@@ -5,6 +9,7 @@ class cfs_apb_sequence_random extends cfs_apb_sequence_base;
   
   rand int unsigned num_items;
   
+  //Constraint on number of times we call the simple sequence
   constraint num_items_default{
     soft num_items inside {[1:10]};  
   }
@@ -19,6 +24,7 @@ class cfs_apb_sequence_random extends cfs_apb_sequence_base;
     for(int i=0; i<num_items; i++) begin
       cfs_apb_sequence_simple seq;
       
+      //uvm_do can take input argument as sequence item or a sequence itself as well
       `uvm_do(seq)
     end
     
